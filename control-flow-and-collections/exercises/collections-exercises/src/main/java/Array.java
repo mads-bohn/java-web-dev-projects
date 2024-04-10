f7a1cc2514f21404b734be6deb564c783fcf076f
mads-bohn <mady.bohn@gmail.com>
2024-04-10 10:12:07 -0500

    completed exercises

---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/Array.java
@@ -0,0 +1,12 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+
+        for (i = 0; i < integerArray.length; i+2) {
+            System.out.println(integerArray[i]);
+        }
+
+    }
+
+}
